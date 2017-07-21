c963da32c4a98da811b016e40f791e2c6e54e2de
sriblessy <30342992+sriblessy@users.noreply.github.com>
2017-07-21 17:33:49 +0530

    Create sri.c

---
+++ sri.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\nEnter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("\nGiven number is positive");
+}
+else
+{
+printf("\nGiven number is negative")
+}
+else
+{
+printf("\n given number is zero");
+}
+}
